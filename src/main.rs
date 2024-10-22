3aa52004e3661c17e364c31d38e112a8a1f64005
Dasun Pubudumal <pubudumald@gmail.com>
2024-10-22 15:31:18 +0100

    Integrating Tokio runtime and sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+use sqlx::{Connection, Executor, SqliteConnection};
+use sqlx::sqlite::SqliteRow;
+use sqlx::Row;
+
 mod models;
 
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), sqlx::Error> {
+
+    let mut conn = SqliteConnection::connect("sqlite::memory:").await?;
+
+    let row: SqliteRow = sqlx::query("SELECT ?")
+        .bind(150_i64)
+        .fetch_one(&mut conn).await?;
+
+    let row = row.get::<i64, _>(0);
+
+    assert_eq!(row, 150);
+
+    Ok(())
 }
