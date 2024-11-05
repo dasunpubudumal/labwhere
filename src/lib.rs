5fd73942cd39ac74b3af96338ad068d6acf2be5e
Dasun Pubudumal <pubudumald@gmail.com>
2024-11-05 09:42:34 +0000

    Organize module declarations in lib.rs and update main.rs to use the new module paths

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+// Declare modules here (not in main.rs).
+// And use them in main.rs like `use labwhere::<module-name>`
+// e.g. `use labwhere::models::location_type::LocationType`
+pub mod db;
+pub mod models;
