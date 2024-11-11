773df16b77d26238ea0d234007986f89c65cf8be
Dasun Pubudumal <pubudumald@gmail.com>
2024-11-11 11:54:57 +0000

    [skip ci] Enhance documentation on module visibility and hierarchy in main and models files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use labwhere::db::init_db;
 use labwhere::models::location_type::LocationType;
 
+// Any module that is imported into here (e.g., `use abc_module;`) has its ancestry as the binary
+// crate. Therefore, any function that is declared in the module (e.g., `abc_module`) under `pub(crate)`
+// visibility can be accessed by the binary crate and NOT the library crate. If the module needs to be accessed
+// by both crates, it needs to be made `pub`. The binary crate depends on the library crate (which has the same 
+// name listed in Cargo.toml).
+
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     // Another option is to use SqlitePool. A pool gives a bunch of active connections and will
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,5 @@
+// Module hierarchy of this module is as follows.
+// lib -> models -> (descendant e.g., labware)
 mod labware;
 mod location;
 pub mod location_type;
