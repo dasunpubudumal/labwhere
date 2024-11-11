6921ec732c22c2863a0a92be70e3d05173c6d5f6
Dasun Pubudumal <pubudumald@gmail.com>
2024-11-11 11:39:30 +0000

    [skip ci] Documenting lib and main rust files.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 // Include new module declarations here instead of in main.rs.
 // And use them in main.rs using the module root `labwhere`.
 // e.g. use labwhere::models::location_type::LocationType;
+//
+// The project having both main.rs and lib.rs means that it has two crates - a binary and a library.
+// Both of these crates have the same name as the package listed in Cargo.toml.
+//
+// For more info, check https://doc.rust-lang.org/book/ch07-01-packages-and-crates.html.
 pub mod db;
 pub mod models;
