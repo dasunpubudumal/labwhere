9a0309ff82cd8b90e9a95b4bfd7bd04d1970a610
Dasun Pubudumal <pubudumald@gmail.com>
2024-12-12 17:21:21 +0530

    Running cargo fmt

--- src/services/scan.rs
+++ src/services/scan.rs
@@ -1,17 +1,15 @@
-use http_body_util::{BodyExt, Empty};
 use http_body_util::combinators::BoxBody;
-use hyper::{Request, Response, StatusCode};
+use http_body_util::{BodyExt, Empty};
 use hyper::body::Bytes;
 use hyper::Method;
+use hyper::{Request, Response, StatusCode};
 
 /// Receives location barcode and labware, scans them into LabWhere.
 pub async fn scan(
     req: Request<hyper::body::Incoming>,
 ) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
     match (req.method(), req.uri().path()) {
-        (&Method::POST, "/scan") => {
-          Ok(Response::new(req.into_body().boxed()))
-        },
+        (&Method::POST, "/scan") => Ok(Response::new(req.into_body().boxed())),
         _ => {
             let mut not_found = Response::new(empty());
             *not_found.status_mut() = StatusCode::NOT_FOUND;
