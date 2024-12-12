ba24967b1ef14363418acce1a4895cf5f99a96b0
Dasun Pubudumal <pubudumald@gmail.com>
2024-12-12 16:48:03 +0530

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,19 @@
 // by both crates, it needs to be made `pub`. The binary crate depends on the library crate (which has the same
 // name listed in Cargo.toml); because stuff from library crate are imported in line 1 and 2.
 
-use std::convert::Infallible;
-use hyper::body::Bytes;
-use std::net::SocketAddr;
 use http_body_util::Full;
+use hyper::body::Bytes;
 use hyper::body::Incoming;
-use hyper::{Request, Response};
-use hyper_util::rt::TokioIo;
-use tokio::net::TcpListener;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-
     // Bind the server to an address
     let address = SocketAddr::from(([127, 0, 00, 1], 3000));
 
@@ -31,9 +30,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
         let io = TokioIo::new(stream);
 
-        // Spaw na tokio task for concurrent processing of incoming streams
+        // Spawn tokio task for concurrent processing of incoming streams
         tokio::task::spawn(async move {
-            if let Err(err) = http1::Builder::new().serve_connection(io, service_fn(hello)).await {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
                 eprintln!("Error serving the connection: {:?}", err);
             }
         });
