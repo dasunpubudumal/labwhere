52135049e3f0da99a6d334931f5adfd0fcd37f3b
yoldas <ay6@sanger.ac.uk>
2025-02-10 15:26:45 +0000

    Modify scan create method with the check for the existence of location

--- src/models/scan.rs
+++ src/models/scan.rs
@@ -1,7 +1,9 @@
+use crate::errors::NotFoundError;
+use crate::models::location::Location;
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use sqlx::sqlite::SqliteConnection;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Scan {
     labware_barcode: String,
     location_barcode: String,
@@ -22,9 +24,21 @@ impl Scan {
     /// depending on the error type.
     /// 3. Find the labware by its barcode `labware_barcode`.
     /// 4. If the labware exists, return it. If it doesn't exist, create the labware in the database.
-    pub fn create(value: Value) -> Result<Scan, ()> {
-
+    pub async fn create(
+        scan: Scan,
+        connection: &mut SqliteConnection,
+    ) -> Result<Scan, NotFoundError> {
         // TODO: Complete this function.
+
+        // let location_result = Location::find_by_barcode(scan.location_barcode);
+        let location: Location =
+            match Location::find_by_barcode(scan.location_barcode, connection).await {
+                Ok(location) => location,
+                Err(error) => {
+                    // Respond with a 404
+                    return Err(error);
+                }
+            };
 
         Ok(Scan {
             labware_barcode: "".to_string(),
@@ -35,6 +49,7 @@ impl Scan {
 
 #[cfg(test)]
 mod tests {
+    use crate::db::init_db;
     use crate::models::scan::Scan;
 
     #[test]
@@ -42,5 +57,14 @@ mod tests {
         let scan = Scan::new("lw-bc-1".to_string(), "lc-bc-1".to_string());
         assert_eq!(scan.location_barcode, "lc-bc-1".to_string());
         assert_eq!(scan.labware_barcode, "lw-bc-1".to_string());
+    }
+
+    #[tokio::test]
+    async fn test_scan_create_no_location() {
+        let scan = Scan::new("lw-bc-1".to_string(), "lc-bc-1".to_string());
+        let mut connection = init_db("sqlite::memory:").await.unwrap();
+        Scan::create(scan, &mut connection)
+            .await
+            .expect_err("Location not found");
     }
 }
